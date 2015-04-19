ed31ff9e77c29d44ba6c4b958138c8e911dc3e64
J Gao <jianxin@MacBook-Pro.local>
2015-04-19 16:28:41 -0700

    minor

--- customer_mysql.sql
+++ customer_mysql.sql
@@ -5,5 +5,4 @@ INSERT INTO customer VALUES
 ('chrishou118@gmail.com', 'Yutao', 'Hou', 'SJSU', '1 Washington Sq, San Jose, CA 95192', '4089241001'),
 ('chienshin1@gmail.com', 'Jianxin', 'Gao', 'SJSU', '1 Washington Sq, San Jose, CA 95192', '4089241002'),
 ('ambersun1234@gmail.com', 'Chiunghui', 'Tseng', 'SJSU', '1 Washington Sq, San Jose, CA 95192', '4089241003'),
-('nan.ding@gmail.com', 'Nan', 'Ding', 'SJSU', '1 Washington Sq, San Jose, CA 95192', '4089241004')
-;
+('nan.ding@gmail.com', 'Nan', 'Ding', 'SJSU', '1 Washington Sq, San Jose, CA 95192', '4089241004');
