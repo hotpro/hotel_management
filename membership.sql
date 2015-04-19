d30e621a6f367fba098bec1d554f3c1e2308fa4f
J Gao <jianxin@MacBook-Pro.local>
2015-04-19 16:40:02 -0700

    inserting membership table

---
+++ membership.sql
@@ -0,0 +1,6 @@
+INSERT INTO membership VALUES 
+(10001, 10000, 1, 'carolihuan@gmail.com'),
+(10002, 20000, 2, 'chrishou118@gmail.com'),
+(10003, 12000, 1, 'chienshin1@gmail.com'),
+(10004, 34000, 3, 'ambersun1234@gmail.com'),
+(10005, 46000, 4, 'nan.ding@gmail.com');
