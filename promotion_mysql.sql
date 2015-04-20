b0d13056ae1fd68555e7557cf5207963b9b67bf5
li-huan <lihuan921@gmail.com>
2015-04-19 20:44:43 -0700

    add promo_room, change the key of the promotion to promo_name

--- promotion_mysql.sql
+++ promotion_mysql.sql
@@ -1,10 +1,25 @@
 use hotel;
 
 INSERT INTO promotion VALUES
-(1, 'AAA', 2000-01-01, 2100-01-01, 0.1, 0, 1),
-(2, 'Government', 2000-01-01, 2100-01-01, 0.15, 0, 1),
-(3, 'Senior',2000-01-01, 2100-01-01, 0.15, 0, 1),
-(4, 'Member', 2000-01-01, 2100-01-01, 0, 100, 3),
-(5, 'Member promotion', 2015-04-15, 2015-07-15, 0, 200, 2),
-(6, 'Spring promotion', 2015-03-01, 2015-04-15, 0.2, 0, 3),
-(7, 'May promotion', 2015-05-01, 2015-05-30, 0.1, 0, 3);
+('AAA', '2000-01-01', '2100-01-01', 0.1, 0, 1),
+('Government', '2000-01-01', '2100-01-01', 0.15, 0, 1),
+('Senior','2000-01-01', '2100-01-01', 0.15, 0, 1),
+('Member', '2000-01-01', '2100-01-01', 0.1, 100, 2),
+('2015 Member summer promo', '2015-04-15', '2015-08-15', 0, 300, 3),
+('2015 Spring promo', '2015-03-01', '2015-04-15', 0.2, 0, 3),
+('2015 May promo', '2015-05-01', '2015-05-30', 0.1, 0, 3),
+('2015 May Ballroom promo', '2015-05-01', '2015-05-20', 0.1, 0, 3);
+
+INSERT INTO promo_room VALUES
+('Standard', 'AAA'),
+('Standard', 'Government'),
+('Standard', 'Senior'),
+('Standard', 'Member'),
+('Standard', '2015 Member summer promo'),
+('Standard', '2015 Spring promo'),
+('Standard', '2015 May promo'),
+('Ballroom', '2015 May Ballroom promo'),
+('Ballroom', 'AAA');
+
+
+
